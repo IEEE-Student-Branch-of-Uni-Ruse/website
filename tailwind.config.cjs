d78b9bd23b3b69c8510c5d69373267c07ff63a85
Gabriel Kanev <contact@mrgkanev.eu>
2023-08-29 13:53:08 +0300

    wip

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,8 @@ module.exports = {
         primary: 'var(--aw-color-primary)',
         secondary: 'var(--aw-color-secondary)',
         accent: 'var(--aw-color-accent)',
-        'ieeecolor': '#0A70A3'
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans)', ...defaultTheme.fontFamily.sans],
